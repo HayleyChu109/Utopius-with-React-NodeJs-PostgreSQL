7612f80fd40b9163c90dd1961c6b4a8e10bde2a1
johnjaller <johnjaller@hotmail.com>
2021-11-16 09:20:11 +0800

    Added migration files

---
+++ backend/migrations/20211116011502_redeem_detail.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("redeem_detail", (table) => {
+        table.increments()
+        table.integer('token_transaction_id').unisgned()
+        table.foreign("token_transaction_id").references("tokenTransaction.id");
+        table.integer('redeem_item_id').unisgned()
+        table.foreign("redeem_item_id").references("redeem_item.id");
+        table.integer('amount')
+        table.string('status')
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("redeem_detail");
+  
+};
