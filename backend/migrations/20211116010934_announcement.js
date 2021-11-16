56aceb82b0fca85fbcdcf720e26d7c02d288bb46
johnjaller <johnjaller@hotmail.com>
2021-11-16 10:21:44 +0800

    Added migration files

--- backend/migrations/20211116010934_announcement.js
+++ backend/migrations/20211116010934_announcement.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("announcement", (table) => {
+    table.increments();
+    table.string("title");
+    table.json("content");
+    table.date("start_date");
+    table.date("end_date");
+    table.boolean("isPrivate");
+    table.timestamps(false, true);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("announcement");
 };
